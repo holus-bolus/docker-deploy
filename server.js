13085c41df14ae698301232b45ed2516afad7b93
holus-bolus <ustinovoleksij@gmail.com>
2025-02-26 17:40:21 +0200

    Changed the port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
-
+const PORT = process.env.PORT || 5000
 app.post('/user', (req, res, next) => {
 	res.send('<h1>' + req.body.username + '</h1>');
 });
@@ -14,6 +14,6 @@ app.get('/', (req, res, next) => {
 	res.end('<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>')
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
 	console.log('Server is running on port 5000');
 })
